5706ac851df4f0feb2fb8ce0c591a5d827f80020
Administrator <398741302@qq.com>
2020-01-31 22:03:46 +0800

    add requirements

--- django_vue_cmdb/urls.py
+++ django_vue_cmdb/urls.py
@@ -1,4 +1,4 @@
-"""django_vue URL Configuration
+"""django_vue_cmdb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- django_vue_cmdb/wsgi.py
+++ django_vue_cmdb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_vue project.
+WSGI config for django_vue_cmdb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_vue.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_vue_cmdb.settings')
 
 application = get_wsgi_application()
